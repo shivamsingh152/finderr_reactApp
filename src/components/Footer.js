ff0a2fb94ce074bb7f0b2b33b4fbe75522eeb395
shivam singh <shivamsingh29626@gmail.com>
2023-11-21 20:41:06 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <hr />
     <div className="footer">
     <span>
     <i className="fa fa-sort"></i> Sort | <i class="fa fa-filter"></i> Filter
      <i class="fa-solid fa-bars-filter"></i>
      </span>
     </div>
-    <hr />
     <div>
      <span className='heading'>Finderr</span>    
    </div> 
